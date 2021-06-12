d7f6f2c654f6f44963c2c71526e7a8069c39be90
RozanMagdy <rozanabdelmawla@gmail.com>
2021-06-12 15:02:33 +0200

    added control lab 2

--- Control_Lab2/first_pkg/first_pkg/sub.py
+++ Control_Lab2/first_pkg/first_pkg/sub.py
@@ -45,39 +45,19 @@ class my_node(Node):
         super().__init__("control_node")
         self.get_logger().info("Control_node is Started")
         self.start = timeit.default_timer()
-        #-----------------PUB---------
-        self.create_timer(2,self.timer_call_pub)
-        self.obj_pub_1=self.create_publisher(String,"PWM",10)
-        #---------------SUB-----------        
-        self.create_subscription(Twist,"cmd_vel",self.timer_call_sub,10)
-        #-------------serial---------
-        self.ser = serial.Serial('/dev/ttyACM0', 9600) 
-        #-------------------
+        self.create_timer(1/50,self.timer_call_back)
         self.stop = timeit.default_timer()
+        self.create_subscription(Twist,"cmd_vel",self.Sub_call_back,10)
+        self.ser = serial.Serial('/dev/ttyACM0', 9600) 
+       
   
-       
-       
-       
-    
-    def timer_call_pub(self):
-        #-------------
-        PIDmsg=String()
-        PIDmsg.data = str(self.rightPID)+","+str(self.LeftPID)
-        self.obj_pub_1.publish(PIDmsg)
-        self.get_logger().info(PIDmsg.data)
-        self.ser.write(PIDmsg.data)
-        
-
-        
-                   
-               
-    def timer_call_sub(self,Twist_msg):
-        Velocities= self.ser.readline().split(",")
-        self.Vleft_fb= int(Velocities[0])
-        self.Vright_fb= int(Velocities[1])
-
-        self.angular_velocity=Twist_msg.angular.z
-        self.liner_velocity=Twist_msg.linear.x
+    def timer_call_back(self):
+        try :
+            Velocities= self.ser.readline().split(",")
+            self.Vleft_fb= int(Velocities[0])
+            self.Vright_fb= int(Velocities[1])
+        except:
+            print("Serail input is not available") 
 
         self.Vleft_calc= ((2*self.liner_velocity)-(self.length*self.angular_velocity))/2
         self.Vright_calc= ((2*self.liner_velocity)+(self.length*self.angular_velocity))/2
@@ -86,7 +66,7 @@ class my_node(Node):
         error_in_Vright=abs( self.Vright_fb-self.Vright_calc)
 
         elapsedTime= self.stop-self.start
-        
+
         self.LeftCum_Error += error_in_Vleft * elapsedTime
         self.LeftRate_Error = (error_in_Vleft - self.LeftlastError)/elapsedTime
 
@@ -95,10 +75,26 @@ class my_node(Node):
 
         self.LeftPID=(self.LeftKP*(error_in_Vleft))+(self.LeftKI*(self.LeftCum_Error)+(self.LeftKD*self.LeftRate_Error))
         self.rightPID=(self.RightKP*(error_in_Vright))+(self.RightKI*(self.RightCum_Error)+(self.RightKD*self.RightRate_Error))
+        if(self.LeftPID>255):
+            self.LeftPID=255
+        if (self.rightPID >255):
+            self.rightPID =255
 
+        PIDmsg = str(self.rightPID)+","+str(self.LeftPID)
+        try:
+            self.ser.write(PIDmsg)
+        except:
+             print("Serail output is not available") 
 
         self.LeftlastError=error_in_Vleft
         self.RightlastError=error_in_Vright
+
+
+    def Sub_call_back(self,Twist_msg):
+        self.angular_velocity=Twist_msg.angular.z
+        self.liner_velocity=Twist_msg.linear.x
+
+        
 
 
 
